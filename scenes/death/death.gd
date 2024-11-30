3f05aa8d98ada84eed8b399635818790e7d183be
ReneyGG <jakubczyzycki12@gmail.com>
2024-11-30 17:40:36 +0100

    deathscreen

---
+++ scenes/death/death.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("powerpoint")
+
+func _on_retry_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/test/test.tscn")
+
+func _on_menu_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/gui/main_menu.tscn")
