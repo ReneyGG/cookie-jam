5a80003104e25ce48851c747f7e84dadc09eb9f9
ReneyGG <jakubczyzycki12@gmail.com>
2024-11-30 18:05:23 +0100

    death screen fin

--- scenes/death/death.gd
+++ scenes/death/death.gd
@@ -2,7 +2,9 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Screens.visible = true
 	$AnimationPlayer.play("powerpoint")
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_retry_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/test/test.tscn")
